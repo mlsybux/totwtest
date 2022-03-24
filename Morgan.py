cb95b0f493c17852d7cba91f1b9cdb47bcd85b88
KingJad <jleceaalmendral-15@spotsylvania.k12.va.us>
2022-03-24 13:32:16 -0400

    AAAAA

---
+++ Morgan.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+class Morgan:
+    def __init__(self, master):
+        self.master = master
+
+    #everything under HERE is just user interface/GUI/visual stuff that gets put up on screen :)
+
+    #everything under here is just calculations
+    def test_wood(self, days):
+        return (days*5)
